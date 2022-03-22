cc1b0024515a66d32667cde91f1e0a8e4e3f1e05
woxxel <alexander.wollex.schmidt@googlemail.com>
2022-03-22 11:41:18 +0100

    small changes to plotting

--- two_populations.py
+++ two_populations.py
@@ -20,19 +20,31 @@ def two_populations(steps=100,plot_ax3D=True,save=0,file_format='png',rerun=Fals
     ## general plot setup
     set_plot_params()
 
-    fig,ax = plt.subplots(2,3,figsize=(7.5,7.5),dpi=300)
+    fig,ax = plt.subplots(2,3,figsize=(7.5,5),dpi=300)
     plt_para = {
         'ax_label': [],
         'const_label': []
     }
 
+    # options = {
+    #     'mode_stats': 0,
+    #     'order': ['rateWnt','n','alpha_0','tau_G','eta','eps'],
+    #     'eps': [1./np.sqrt(2)],
+    #     'alpha_0': [0.02],
+    #     'rateWnt': [0.,20.],
+    #     'tau_G': [0.01],
+    #     'tau_A': [0.005],
+    #     'Npop': 2,
+    #     'eta': [0.9],
+    #     'n': [0.,1.],
+    # }
     options = {
         'mode_stats': 0,
-        'order': ['tau_G','n','alpha_0','rateWnt','eta','eps'],
-        'eps': [1./np.sqrt(2)],
-        'alpha_0': [0.02],
+        'order': ['eps','alpha_0','rateWnt','n','tau_G','eta'],
+        'eps': [0,1./np.sqrt(2)],
+        'alpha_0': [0,0.1],
         'rateWnt': [1.],
-        'tau_G': [0.,0.1],
+        'tau_G': [0.03],
         'tau_A': [0.005],
         'Npop': 2,
         'eta': [0.9],
@@ -73,15 +85,34 @@ def two_populations(steps=100,plot_ax3D=True,save=0,file_format='png',rerun=Fals
         plot_fins(ax[p,0],res[options['order'][0]],res[options['order'][1]],res['gamma'][p,...],res['chi'][p,...],res['regions'][p,...],plt_para)
 
     options['rateWnt'] = [2.]
+    # options['tau_G'] = [0.03]
     res = darkMatter(steps=steps,options=options,rerun=rerun,compile=compile)
     for p in range(2):
         plot_fins(ax[p,1],res[options['order'][0]],res[options['order'][1]],res['gamma'][p,...],res['chi'][p,...],res['regions'][p,...],plt_para)
 
     options['rateWnt'] = [5.]
+    # options['tau_G'] = [0.06]
     res = darkMatter(steps=steps,options=options,rerun=rerun,compile=compile)
     for p in range(2):
         plot_fins(ax[p,2],res[options['order'][0]],res[options['order'][1]],res['gamma'][p,...],res['chi'][p,...],res['regions'][p,...],plt_para)
 
+
+    big_ax = fig.add_axes([0.1,0.1,0.8,0.85])
+    big_ax.set_facecolor('none')
+    big_ax.tick_params(labelcolor='none',top=False,bottom=False,left=False,right=False)
+    big_ax.spines['top'].set_visible(False)
+    big_ax.spines['right'].set_visible(False)
+    big_ax.spines['bottom'].set_visible(False)
+    big_ax.spines['left'].set_visible(False)
+
+    # plt.setp(big_ax,xlabel=r'$\displaystyle \bar{\nu}$',ylabel=r'$\displaystyle r$')
+    plt.setp(big_ax,xlabel=r'$\displaystyle \bar{\eps}$',ylabel=r'$\displaystyle \alpha_0$')
+
+    fig.tight_layout(pad=3.0)
+    if save:
+        sv_name = './figures/two_pop_n.%s' % (file_format)
+        plt.savefig(sv_name,format=file_format,dpi=600)
+        print('Figure saved as "%s"' % sv_name)
 
     plt.show(block=False)
 
