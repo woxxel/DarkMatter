3256072d3cf2fd158f4c4c60c5e66d07b6a221d7
woxxel <alexander.wollex.schmidt@googlemail.com>
2022-09-18 11:26:53 +0200

    pushing read mat to .py file

--- empirical/read_data.py
+++ empirical/read_data.py
@@ -7,14 +7,11 @@ import h5py
 def compDiff(a,b,classify):
     comp = True
     for c in classify:
-        #print(a[c],b[c])
         if type(a[c])==np.ndarray:
             comp = comp and np.array_equal(a[c],b[c])
-            #print('is array')
         else:
             comp = comp and a[c]==b[c]
-        #print(comp)
-    #print('overall:',comp)
+
     return comp
 
 
@@ -42,7 +39,7 @@ def plot_animal(spike_data,animal,ax,j=0):
             i+=1
     plt.tight_layout()
 
-def read_data(filePath='../data/BuscheLab/spiking_data_for_modeling.mat', classify = ['spikes_animal_idx','spikes_hemisphere_idx','spikes_genotype'],plot=False):
+def read_data(filePath='../data/BuscheLab/spiking_data_for_modeling_with_depth.mat', classify = ['spikes_animal_idx','spikes_hemisphere_idx','spikes_genotype'],plot=False):
 
     """
 
@@ -68,7 +65,16 @@ def read_data(filePath='../data/BuscheLab/spiking_data_for_modeling.mat', classi
     data['spikes_animal_idx'] = data['spikes_animal_idx'].flatten()
     data['spikes_cluster_idx'] = data['spikes_cluster_idx'].flatten()
     data['spikes_hemisphere_idx'] = data['spikes_hemisphere_idx'].flatten()
-    # data['spikes_depth_rel2L5'] = data['spikes_depth_rel2L5'].flatten()
+    data['spikes_depth_rel2L5'] = data['spikes_depth_rel2L5'].flatten()
+
+    ### Compart neurons into layers
+    layer_border = [-1000,-250,200,1000]
+    depth = np.array(data['spikes_depth_rel2L5'])
+    data['layer'] = np.zeros((len(depth)),'int')
+    for l in range(len(layer_border)-1):
+        #print((layer_border[l-1] < depth) & (depth < layer_border[l]))
+        data['layer'][(layer_border[l] <= depth) & (depth < layer_border[l+1])] = l
+        print(l,layer_border[l],layer_border[l+1], sum(data['layer']==l))
 
     ### clustering data
     spike_data = []
@@ -78,12 +84,10 @@ def read_data(filePath='../data/BuscheLab/spiking_data_for_modeling.mat', classi
         tmp_class = {}
         for c in classify:
             tmp_class[c] = data[c][n]
-        #print(tmp_class)
 
         ### search, whether this set of data already exists
         idx = None
         for i,sd in enumerate(spike_data):
-            #print('classification',sd['classification'])
             if compDiff(tmp_class,sd['classification'],classify):
                 idx = i
                 break
@@ -96,11 +100,15 @@ def read_data(filePath='../data/BuscheLab/spiking_data_for_modeling.mat', classi
                 'spike_times': [],
                 'neuron_idx': [],
                 'cluster_idx': [],
+                'layer': [],
+                'depth': [],
             })
 
         spike_data[idx]['spike_times'].append(np.array(f[data['spikes_timestamps'][n][0]])[0])
         spike_data[idx]['neuron_idx'].append(n)
         spike_data[idx]['cluster_idx'].append(data['spikes_cluster_idx'][n])
+        spike_data[idx]['layer'].append(data['layer'][n])
+        spike_data[idx]['depth'].append(data['spikes_depth_rel2L5'][n])
 
 
     ### some processing
@@ -145,26 +153,58 @@ def read_data(filePath='../data/BuscheLab/spiking_data_for_modeling.mat', classi
             h = int(sd['classification']['spikes_hemisphere_idx'])-1
             r[a,h] = sd['g'][2]/np.sum(sd['g'][1:])
             N[a,h] = sd['N']
-        plt.plot(N[:,0],r[:,0],'ro',label='left')
-        plt.plot(N[:,1],r[:,1],'ko',label='right')
-        plt.plot(N.T,r.T,'k-')
+
+        fig, ax = plt.subplots(2,2,figsize=(10,8))
+        ax[0][0].plot(N[:,0],r[:,0],'ro',label='left')
+        ax[0][0].plot(N[:,1],r[:,1],'ko',label='right')
+        ax[0][0].plot(N.T,r.T,'k-')
 
         #print('fraction of inhibitory neurons: ', r)
-        plt.ylabel('fraction of inhibitory neurons')
-        plt.xlabel('total number of neurons')
-        plt.legend()
+        plt.setp(ax[0][0],ylabel='fraction of inhibitory neurons',xlabel='total number of neurons')
+        ax[0][0].legend()
+
+
+        N = np.zeros((nAnimals,3,3))
+
+        for a in range(nAnimals):
+            for h in range(2):
+                for l in range(3):
+                    for c in range(3):
+                        for sd in spike_data:
+                            if int(sd['classification']['spikes_animal_idx'])-1==a:
+                                idx = (np.array(sd['layer'])==l) & (sd['cluster_idx']==c)
+                                N[a,l,c] += idx.sum()
+
+        ax[0][1].plot(N[:,0,1],N[:,0,2],'o',color=[0,0,0.2],label='L1')
+        ax[0][1].plot(N[:,1,1],N[:,1,2],'o',color=[0,0,1],label='L2')
+        ax[0][1].plot(N[:,2,1],N[:,2,2],'o',color=[0.7,0.7,1],label='L3')
+        plt.setp(ax[0][1],xlabel='excitatory',ylabel='inhibitory')
+
+        ax[0][1].legend()
+
+
+        _ = ax[1][0].hist(data['spikes_depth_rel2L5'],np.linspace(-1000,1000,101))
+        plt.setp(ax[1][0],xlabel='depth',ylabel='# neurons')
         plt.show(block=False)
 
-        def change_slider(animal):
-            print(animal)
-            plot_animal(spike_data,animal,ax)
+        #fig,ax = plt.subplots(4,5,figsize=(15,8))
+        for i,animal in enumerate(spike_data):
+            _ = ax[i//5][i%5].hist(animal['depth'],np.linspace(-1000,1000,51))
+
+
+        plt.show(block=False)
 
         animal = 2
         fig,ax = plt.subplots(2,2,figsize=(12,4))
-        # axamp = plt.axes([0.02, .6, 0.4, 0.02])
+
+        def change_slider(animal):
+            # print(animal)
+            plot_animal(spike_data,animal,ax)
+
+        axamp = plt.axes([0.02, .6, 0.4, 0.02])
         plot_animal(spike_data,animal,ax,4)
-        # slider = Slider(axamp, 'animal', 1, 9, valinit=animal,orientation='horizontal')
-        # slider.on_changed(change_slider)
+        slider = Slider(axamp, 'animal', 1, 9, valinit=animal,orientation='horizontal')
+        slider.on_changed(change_slider)
         plt.show(block=False)
 
     return spike_data
