Working stage of the inference with multiple conditions - introduced network class for in-python solution to the selfconsistency equation - fixed BayesModel to properly calculate loglikelihood - introduced plots for multiple conditions, comparing separate, joint and theoretical handling of multiple conditions

darkMatter.py
@@ -233,7 +233,7 @@ def set_simulation(fileSim,options,steps):
     )  # 0=phase plots, 1=data simulation
     sim.set_para("mode_calc", 0, options, np.int16, register=False)  # 0=exact, 1=approx
     sim.set_para(
-        "mode_stats", 0, options, np.int16, register=False
+        "mode_stats", 1, options, np.int16, register=False
     )  # 0=general stats, 1=...
     sim.set_para(
         "mode_selfcon", 0, options, np.int16, register=False
@@ -256,7 +256,7 @@ def set_simulation(fileSim,options,steps):
                 # sv_str += '_%s=%g' %^(key,val[-1])
                 sv_str += f'_{key}'
 
-    # sim.print_parameter()
+    sim.print_parameter()
 
     return sim, sv_str
 

empirical/create_surrogate_data.py
@@ -74,9 +74,9 @@ class SurrogateData:
         i = -1
         while True:
             low = 10**i
-
+            # print(i)
             res,err = quad(f_target,0,low,args=args,points=np.logspace(-3,0,4))
-            if res < tolerance:
+            if res < tolerance or i<-12:
                 break
 
             i -= 1
@@ -86,7 +86,7 @@ class SurrogateData:
         nu = np.logspace(np.log10(low),np.log10(high),10**3+1)
 
         M = np.ceil(np.nanmax(f_target(nu,*args) / normalized_envelope_density(nu,low,high)))
-
+        M = min(M,100)
         samples = rejection_sampling(f_target,args,self.N, M, low, high)
         # print("samples:",samples.shape,samples.max(),samples)
         samples_T = np.random.poisson(samples*self.T,samples.shape)
@@ -94,7 +94,7 @@ class SurrogateData:
 
         if plot:
             nbins = max(101,self.N//20)
-            fig,ax = plt.subplots(1,2,figsize=(6,3))
+            fig,ax = plt.subplots(1,2,figsize=(4,2))
             ax[0].hist(samples,bins=np.logspace(np.log10(low),np.log10(high),nbins),density = True,label='samples',color="tab:blue",alpha=0.5)
             ax[0].hist(samples_T/self.T,bins=np.logspace(np.log10(low),np.log10(high),nbins),density = True,label='samples(T)',color="tab:red",alpha=0.5)
             ax[0].plot(nu, f_target(nu,*args),'k-',label='target f')
@@ -150,7 +150,7 @@ class SurrogateData:
         nbins = 101
         n_steps = 10001
 
-        print(f"{self.n_pop=}")
+        # print(f"{self.n_pop=}")
         colors = ['k','tab:red']
         if param:
             # bins = np.linspace(0, param["distr"][0]["nu_max"], 101)
@@ -215,7 +215,9 @@ def sample_from_normalized_envelope(low, high):
 
 def rejection_sampling(f_target,args,N, M, low, high):
     samples = []
+    # print(f"{M=}, {low=}, {high=}")
     while len(samples) < N:
+        # print("samples:",len(samples))
         nu = sample_from_normalized_envelope(low, high)
         v = np.random.uniform(0, 1)
         if v <= f_target(nu,*args) / (M * normalized_envelope_density(nu, low, high)):

inference/BayesModel.py
@@ -127,29 +127,37 @@ class BayesModel(HierarchicalModel):
                     """
 
                     full_idx = (chain,) + idx
-
+    
                     params = self.get_params_from_p(p_in, idx_chain=chain, idx=idx, biological=biological)
                     if not params:
                     # happens if parameters are weird
                         logl[full_idx] = -(10**6)
+                        # print("weird parameters")
                         continue
 
-                    ## calculate maximum number of spikes:
-                    max_spike_count_model = np.squeeze(
-                        params["distr"][0].nu_max * self.T
-                    ).astype("int")
-                    # print(f"{max_spike_count_model=} vs {max_spike_count=}")
-
-                    logl[full_idx], abort = self.parameter_penalties(params, max_spike_count_model, idx)
-                    if abort: 
-                        continue
+                    
 
                     """
                         from here, animal dependent calculations necessary
                         (due to differences in nu_max and thus max_spike_count_model)
                     """
                     for p in range(self.dimensions["n_pop"]):
+
                         idx_population = idx + (p,)
+
+                        ## calculate maximum number of spikes:
+                        max_spike_count_model = np.squeeze(
+                            params["distr"][p].nu_max * self.T
+                        ).astype("int")
+                        # print(f"{max_spike_count_model=} vs {max_spike_count=}")
+
+                        logl_tmp, abort = self.parameter_penalties(max_spike_count_model, idx_population)
+                        logl[full_idx] += logl_tmp
+                        if abort:
+                            print("parameter penalties failed")
+                            continue
+                        
+                        
                         ## remove all spike counts that are higher than max_spike_count_model
                         N_AP_empirical = self.binom[idx_population]["N_AP"][
                             self.binom[idx_population]["N_AP"] < max_spike_count_model
@@ -192,7 +200,10 @@ class BayesModel(HierarchicalModel):
                         # plt.show()
 
 
-                        # print("logl (pre binom)",logl[full_idx])
+                        # print(" ---- logl (pre binom)",logl[full_idx])
+                        # print(log_binom)
+                        # print(np.log(p_N_AP[N_AP]))
+                        # print(np.log(1 - p_N_AP[N_AP]))
 
                         logl[full_idx] += (
                             log_binom
@@ -230,10 +241,12 @@ class BayesModel(HierarchicalModel):
 
                     # self.log.debug((f'logls: {logl_low=}, {logl_intermediate=}, {logl_tail=}'))
                     if not np.isfinite(logl[full_idx]):
+                        # print("is not finite??",logl[full_idx])
                         logl[full_idx] = -(10**6)
 
             if vectorized:
-                return logl.sum(axis=tuple(range(1, self.dimensions["n"])))
+                # print(logl.shape)
+                return logl.sum(axis=tuple(range(1, self.dimensions["n_iter"]+1)))
             else:
                 self.log.debug(("logl:", logl[0, :].sum()))
                 return logl[0, :].sum()
@@ -272,7 +285,7 @@ class BayesModel(HierarchicalModel):
                 )
 
 
-    def parameter_penalties(self,params, max_spike_count_model, idx):
+    def parameter_penalties(self, max_spike_count_model, idx):
                         
         # for pop in params["distr"]:
         #     if not pop.are_values_ok():
@@ -281,18 +294,18 @@ class BayesModel(HierarchicalModel):
         ## calculate penalty for model max spike count less than observed data
         logl_penalty = 0
 
-        for p in range(self.dimensions["n_pop"]):
-            idx_population = idx + (p,)
-            idx_too_high = np.where(self.binom[idx_population]["N_AP"] > max_spike_count_model)[0]
-            for i in idx_too_high:
-                ## get all spike counts higher than allowed by model
-                N_AP_too_high = self.binom[idx_population]["N_AP"][i]
-                k_AP_too_high = self.binom[idx_population]["k_AP"][i]
-                logl_penalty += (
-                    -(10**6)
-                    * k_AP_too_high
-                    * ((N_AP_too_high - max_spike_count_model) / max_spike_count_model) ** 2
-                )
+        # for p in range(self.dimensions["n_pop"]):
+            # idx_population = idx + (p,)
+        idx_too_high = np.where(self.binom[idx]["N_AP"] > max_spike_count_model)[0]
+        for i in idx_too_high:
+            ## get all spike counts higher than allowed by model
+            N_AP_too_high = self.binom[idx]["N_AP"][i]
+            k_AP_too_high = self.binom[idx]["k_AP"][i]
+            logl_penalty += (
+                -(10**6)
+                * k_AP_too_high
+                * ((N_AP_too_high - max_spike_count_model) / max_spike_count_model) ** 2
+            )
         # print(f"logl_penalty: {logl_penalty}")
 
 
@@ -339,10 +352,6 @@ class BayesModel(HierarchicalModel):
 
                 var,(p,s) = parse_name_and_indices(key,["pop","s"])
 
-                print("set params:")
-                print(key,val)
-                print(var,p,s)
-
                 if p is None and s is None:
                     setattr(self.net,var,val)
                 elif p is not None and s is None:
@@ -356,14 +365,13 @@ class BayesModel(HierarchicalModel):
             if not is_ok:
                 return False
 
-            self.net.set_weights()
-            self.net.calculate_sigma_V()
             self.net.solve_selfcon()
 
             params = {}
             for p,pop in enumerate(self.net.populations):
                 for key in ["gamma","delta","nu_max"]:
                     params[f"{key}_pop{p}"] = getattr(pop, key)
+                    # print(f"{key}_pop{p} = {getattr(pop, key)}")
 
             # print("flat:",params)
             params = build_distr_structure_from_params(params, self.parameter_names)

inference/network.py
@@ -4,28 +4,59 @@ import numpy as np
 from scipy.optimize import fsolve
 from .utils.structures import DistributionModelParams as distr_params
 
+import inspect
+
 
 class Network:
 
     """
         ensure, q, sigma_V, alpha, nu_max are only calculated, not set (protected vars, attributes, ...)
     """
+    n_pop = 0
+    
+    _recalculate_sigma_V = True
+    _recalculate_alpha = True
+
+    _counter_sigma_V = 0
+    _counter_alpha = 0
 
     def __init__(self, **kwargs):
-        self.eps = kwargs.get('eps', 1/np.sqrt(2))
-        self.eta = kwargs.get('eta', 0.9)
-
         self.populations = []
-        self.n_pop = 0
+
+        self._eps = kwargs.get('eps', 1/np.sqrt(2))
+        self._eta = kwargs.get('eta', 0.9)
+
+
+    @property
+    def eps(self):
+        return self._eps
+
+    @eps.setter
+    def eps(self, value):
+        self._eps = value
+        self._set_weights()
+
+    @property
+    def eta(self):
+        return self._eta
+
+    @eta.setter
+    def eta(self, value):
+        self._eta = value
+        self._set_weights()
+        
 
     def register_population(self, idx=None,**kwargs):
 
         if idx is not None and (idx<self.n_pop or idx>self.n_pop):
             warnings.warn(f"Requested to add population {idx}, but currently there are {len(self.populations)} populations registered. You can only add the next population in line.", UserWarning)
             return
+        ## check if idx==len(self.populations)
 
-        self.populations.append(self.Population(len(self.populations),**kwargs))
+        self.populations.append(Population(self,**kwargs))
         self.n_pop += 1
+
+        self._set_weights()
 
     def print_current_state(self):
 
@@ -37,7 +68,7 @@ class Network:
         for pop in self.populations:
             pop.print_current_state()
 
-    def set_weights(self):
+    def _set_weights(self):
         
         self.J = np.zeros((self.n_pop,self.n_pop))
         for post in self.populations:
@@ -49,51 +80,68 @@ class Network:
                 elif post.J0 < 0 and pre.J0 > 0:    # J_IE
                     self.J[post.idx,pre.idx] = pre.J0 * pre.tau_M * self.eps
                 elif post.J0 > 0 and pre.J0 > 0:    # J_EE
-                    self.J[post.idx,pre.idx] = pre.J0 * pre.tau_M * self.eta*self.eps
+                    self.J[post.idx,pre.idx] = pre.J0 * pre.tau_M * self.eta * self.eps
+                # print(f"J(p={post.idx},{pre.idx})={self.J[post.idx,pre.idx]}")
 
+        self._recalculate_sigma_V = True
+        self._recalculate_alpha = True
 
     def calculate_sigma_V(self):
 
-        for post in self.populations:
+        if not self._recalculate_sigma_V:
+            return
 
+        for post in self.populations:
             var_V = 0.
             var_V_dot = 0.
             for pre in self.populations:
-                
+                # print(f"in post/pre {post.idx},{pre.idx}")
+
                 for syn in pre.synapses:
                     prefactor = self.J[post.idx,pre.idx]**2 * pre.kappa * pre.nu_bar / (syn.tau_I + post.tau_M)
-
+                    # print(f"J: {self.J[post.idx,pre.idx]}, tau_I: {syn.tau_I}, tau_M: {post.tau_M}")
+                    # print(f"prefactor: {prefactor}")
                     if len(pre.synapses) == 1:
                         tmp_var_V = prefactor/2.
                     elif len(pre.synapses) == 2:
-                        tmp_var_V = prefactor * syn.r_I**2 / 2 + (syn.r_I * (1 - syn.r_I) * syn.tau_I) / (pre.synapses[0].tau_I + pre.synapses[1].tau_I)
+                        tmp_var_V = prefactor * (syn.r_I**2 / 2 + (syn.r_I * (1 - syn.r_I) * syn.tau_I) / (pre.synapses[0].tau_I + pre.synapses[1].tau_I))
                     else:
                         assert False, "not implemented for more than 2 synapses"
-                    
+                    # print(f"tmp_var_V: {tmp_var_V}")
                     var_V += tmp_var_V
                     var_V_dot += tmp_var_V / (syn.tau_I * post.tau_M)
             
             post._sigma_V = np.sqrt(var_V)
             post._nu_max = np.sqrt(var_V_dot/var_V)/(2*math.pi)
+        self._counter_sigma_V += 1
+        self._recalculate_sigma_V = False
 
 
     def calculate_alpha(self):
+
+        if not self._recalculate_alpha:
+            return
+
         ## requires having set/calculated q before
         var_alpha_external = 0. ## can be implemented later
         for post in self.populations:
 
             var_alpha_I = 0.
             for pre in self.populations:
-
+                # print(f"in post/pre {post.idx},{pre.idx}")
+                # print(f"J: {self.J[post.idx,pre.idx]}, kappa: {pre.kappa}, q: {pre.q}, alpha_0: {post.alpha_0}")
                 var_alpha_I += self.J[post.idx,pre.idx]**2 * pre.kappa * pre.q
-
+                # print(f"var_alpha_I: {var_alpha_I}")
 
             post._alpha = np.sqrt(var_alpha_I + var_alpha_external + post.alpha_0**2)
+
+        self._counter_alpha += 1
+        self._recalculate_alpha = False
 
     def solve_selfcon(self):
         
         q0 = np.array([pop.nu_bar**2 for pop in self.populations])
-        q = fsolve(self.selfcon, q0)
+        q = fsolve(self.selfcon, q0, xtol=1e-12)
         for pop in self.populations:
             pop.q = q[pop.idx]
 
@@ -118,7 +166,7 @@ class Network:
     def are_values_ok(self):
         
         attrs = ["eps", "eta"]
-        is_ok = all(getattr(self, attr) is not None and getattr(self, attr) > 0 for attr in attrs)
+        is_ok = all(getattr(self, attr) is not None and getattr(self, attr) >= 0 for attr in attrs)
         if not is_ok:
             return False
 
@@ -134,158 +182,263 @@ class Network:
             distr.append(distr_params(pop.gamma,pop.delta,pop.nu_max,pop.kappa))
         return distr
 
-    class Population:
+class Population:
 
-        _sigma_V = 0.
-        _alpha = 0.
-        _nu_max = 0.
-        _q = 0.
+    _sigma_V = 0.
+    _alpha = 0.
+    _nu_max = 0.
+    _q = 0.
+    
 
-        def __init__(self, idx, **kwargs):
+    def __init__(self, network, **kwargs):
 
-            self.idx = idx
-            self.nu_bar = kwargs.get('nu_bar', 0.)
-            self.alpha_0 = kwargs.get('alpha_0', 0.)
-            
-            self.J0 = kwargs.get('J0', -1.)
+        # print("IMPLEMENT CALCULATING (approximating?) IMPLAUSIBLE REGION")
+        self.synapses = []
 
-            self.kappa = kwargs.get('kappa', 1.)
-            self.Psi_0 = kwargs.get('Psi_0', 0.)
-            self.tau_M = kwargs.get('tau_M', 0.01)
+        # assert isinstance(network, Network), "Population has to be part of a Network class instance!"
+        self.network = network
+        self.idx = len(network.populations)
 
-            self.synapses = []
-
-        @property
-        def q(self):
-            return self._q
+        self.nu_bar = kwargs.get('nu_bar', 0.)
+        self.alpha_0 = kwargs.get('alpha_0', 0.)
         
-        @q.setter
-        def q(self, value):
-            self._q = value
+        self._J0 = kwargs.get('J0', -1.)
 
-        @property
-        def sigma_V(self):
-            return self._sigma_V
+        self.kappa = kwargs.get('kappa', 1.)
+        self.Psi_0 = kwargs.get('Psi_0', 0.)
+        self._tau_M = kwargs.get('tau_M', 0.01)
 
-        @property
-        def alpha(self):
-            return self._alpha
+    @property
+    def nu_bar(self):
+        return self._nu_bar
 
-        @property
-        def nu_max(self):
-            return self._nu_max
-        
-        @property
-        def gamma(self):
-            return self.sigma_V / self.alpha
-        
-        @property
-        def delta(self):
-            return self.I_nu_squared()**0.5 / self.alpha
+    @nu_bar.setter
+    def nu_bar(self, value):
+        self._nu_bar = value
+        self.network._recalculate_sigma_V = True
 
-        @property
-        def chi(self):
-            return -np.log10(self.nu_peak()/self.nu_bar)
-        
-        
-        def are_values_ok(self):
-            attrs = ['nu_bar','alpha_0','kappa','tau_M']
-            is_ok = all(getattr(self, attr) is not None and getattr(self, attr) > 0 for attr in attrs)
-            if not is_ok:
+    @property
+    def alpha_0(self):
+        return self._alpha_0
+    
+    @alpha_0.setter
+    def alpha_0(self, value):
+        self._alpha_0 = value
+        self.network._recalculate_alpha = True
+
+    @property
+    def tau_M(self):
+        return self._tau_M
+    
+    @tau_M.setter
+    def tau_M(self, value):
+        self._tau_M = value
+        self.network._set_weights()
+
+    @property
+    def J0(self):
+        return self._J0
+
+    @J0.setter
+    def J0(self, value):
+        self._J0 = value
+        self.network._set_weights()
+
+    @property
+    def q(self):
+        return self._q
+    
+    @q.setter
+    def q(self, value):
+        self._q = value
+        self.network._recalculate_alpha = True
+
+    @property
+    def sigma_V(self):
+        self.network.calculate_sigma_V()
+        return self._sigma_V
+
+    @property
+    def alpha(self):
+        self.network.calculate_alpha()
+        return self._alpha
+
+    @property
+    def nu_max(self):
+        return self._nu_max
+    
+    @property
+    def gamma(self):
+        return self.sigma_V / self.alpha
+    
+    @property
+    def delta(self):
+        return self.I_nu_squared()**0.5 / self.alpha
+
+    @property
+    def chi(self):
+        return -np.log10(self.nu_peak()/self.nu_bar)
+    
+    
+    def are_values_ok(self):
+        attrs = ['nu_bar','alpha_0','kappa','tau_M']
+        is_ok = all(getattr(self, attr) is not None and getattr(self, attr) > 0 for attr in attrs)
+        if not is_ok:
+            return False
+
+        for syn in self.synapses:
+            if not syn.are_values_ok():
                 return False
+        return True
 
-            for syn in self.synapses:
-                if not syn.are_values_ok():
-                    return False
-            return True
+    def distribution(self,steps=1000):
 
-        def distribution(self,steps=1000):
+        # rate_arr = np.linspace(0,self.nu_max(),steps)
+        rate_ratio = np.linspace(1/steps,1,steps-1) #rate_arr/self.nu_max()
 
-            # rate_arr = np.linspace(0,self.nu_max(),steps)
-            rate_ratio = np.linspace(1/steps,1,steps-1) #rate_arr/self.nu_max()
+        distr = self.gamma/(self.nu_max*np.sqrt(-np.pi*np.log(rate_ratio)))* \
+                np.exp(-self.delta**2/2)*rate_ratio**(self.gamma**2-1)* \
+                np.cosh(self.gamma*self.delta*np.sqrt(-2*np.log(rate_ratio)))
 
-            distr = self.gamma/(self.nu_max*np.sqrt(-np.pi*np.log(rate_ratio)))* \
-                    np.exp(-self.delta**2/2)*rate_ratio**(self.gamma**2-1)* \
-                    np.cosh(self.gamma*self.delta*np.sqrt(-2*np.log(rate_ratio)))
-
-            at_zero = 0 if self.gamma > 1 else np.inf
-            distr = np.insert(distr,0,at_zero)
-            rate_ratio = np.linspace(0,1,steps)
-            return rate_ratio, distr
+        at_zero = 0 if self.gamma > 1 else np.inf
+        distr = np.insert(distr,0,at_zero)
+        rate_ratio = np.linspace(0,1,steps)
+        return rate_ratio, distr
 
 
-        def nu_peak(self):
-            return self.nu_max * np.exp( - (self.gamma**2 * self.delta**2 - 2*(self.gamma**2 - 1) + self.gamma * self.delta *np.sqrt(self.gamma**2 * self.delta**2 - 4*(self.gamma**2 - 1))) / (4 * (self.gamma**2 - 1)**2))
+    def nu_peak(self):
+        return self.nu_max * np.exp( - (self.gamma**2 * self.delta**2 - 2*(self.gamma**2 - 1) + self.gamma * self.delta *np.sqrt(self.gamma**2 * self.delta**2 - 4*(self.gamma**2 - 1))) / (4 * (self.gamma**2 - 1)**2))
 
 
-        def print_current_state(self):
+    def is_selfconsistent_nu(self):
+        return (self.nu_bar/self.nu_max)**2 * (self.alpha**2 + self.sigma_V**2)/self.sigma_V**2 < 1
+    
+    def is_selfconsistent_q(self):
+        return (self.q/self.nu_max**2)**2 * (2 * self.alpha**2 + self.sigma_V**2)/self.sigma_V**2 < 1
+    
+    @property
+    def is_selfconsistent(self):
+        return self.is_selfconsistent_nu() and self.is_selfconsistent_q()
 
-            print(f"Population {self.idx}:")
-            print(f"  nu_bar: {self.nu_bar}")
-            print(f"  alpha_0: {self.alpha_0}")
-            print(f"  q: {self.q}")
-            print("")
-            print(f"  alpha: {self.alpha}")
-            print(f"  sigma_V: {self.sigma_V}")
-            print(f"  nu_max: {self.nu_max}")
-            print("")
-            print(f"  tau_M: {self.tau_M}")
-            print(f"  Psi_0: {self.Psi_0}")
-            print(f"  kappa: {self.kappa}")
-            print(f"  J0: {self.J0}")
-            for syn in self.synapses:
-                syn.print_current_state()
+    @property
+    def is_dark_matter(self):
+        return self.gamma > 1
 
-        def register_synapse(self, idx=None, **kwargs):
+    @property
+    def is_no_peak(self):
+        return (self.gamma * self.delta)**2 - 4*(self.gamma**2 - 1) < 0
 
-            if idx is not None and (idx < len(self.synapses) or idx > len(self.synapses)):
-                warnings.warn(f"Requested to add synapse {idx}, but currently there are {len(self.synapses)} synapses registered. You can only add the next synapse in line.", UserWarning)
-                return
+    @property
+    def is_implausible(self):
+        ## this is an approximation - implement integration?
+        n_steps = 1000
+        rho = self.distribution(n_steps)[1]
+        rho = rho[np.isfinite(rho)]
+        n_steps = len(rho)
+        return rho[int(0.9 * n_steps):].sum()/rho.sum() > 0.1
 
-            self.synapses.append(self.Synapse(len(self.synapses), **kwargs))
-            self.n_synapses = len(self.synapses)
+    def print_current_state(self):
 
-            # self.synapses.append(self.Synapse(len(self.synapses), **kwargs))
-            # pass
+        print(f"Population {self.idx}:")
+        print(f"  nu_bar: {self.nu_bar}")
+        print(f"  alpha_0: {self.alpha_0}")
+        print(f"  q: {self.q}")
+        print("")
+        print(f"  alpha: {self.alpha}")
+        print(f"  sigma_V: {self.sigma_V}")
+        print("")
+        print(f"  tau_M: {self.tau_M}")
+        print(f"  Psi_0: {self.Psi_0}")
+        print(f"  kappa: {self.kappa}")
+        print(f"  J0: {self.J0}")
+        for syn in self.synapses:
+            syn.print_current_state()
+
+    def print_meta_parameter(self):
+        print(f"  Meta parameters for population {self.idx}:")
+        print(f"    gamma: {self.gamma}")
+        print(f"    delta: {self.delta}")
+        print(f"    nu_max: {self.nu_max}")
+
+    def register_synapse(self, idx=None, **kwargs):
+
+        if idx is not None and (idx < len(self.synapses) or idx > len(self.synapses)):
+            warnings.warn(f"Requested to add synapse {idx}, but currently there are {len(self.synapses)} synapses registered. You can only add the next synapse in line.", UserWarning)
+            return
+
+        self.synapses.append(Synapse(self, **kwargs))
+        self.n_synapses = len(self.synapses)
+
+        # self.synapses.append(self.Synapse(len(self.synapses), **kwargs))
+        # pass
+    
+
+    def check_mixture(self):
+
+        r = 0
+        for syn in self.synapses:
+            r += syn.r_I
         
+        assert r==1, "synapse ratio within population {p} has to add up to 1!"
+    
 
-        def check_mixture(self):
+    def I_nu_squared(self):
 
-            r = 0
-            for syn in self.synapses:
-                r += syn.r_I
-            
-            assert r==1, "synapse ratio within population {p} has to add up to 1!"
-        
+        return - (self.alpha**2 + self.sigma_V**2) * np.log( (self.nu_bar/self.nu_max)**2 * (1 + (self.alpha/self.sigma_V)**2) )
 
-        def I_nu_squared(self):
+    
+    def I_q_squared(self):
 
-            return - (self.alpha**2 + self.sigma_V**2) * np.log( (self.nu_bar/self.nu_max)**2 * (1 + (self.alpha/self.sigma_V)**2) )
+        return - (self.alpha**2 + 1./2 * self.sigma_V**2) * np.log( (self.q/self.nu_max**2)**2 * (1 + 2*(self.alpha/self.sigma_V)**2) )
 
-        
-        def I_q_squared(self):
-
-            return - (self.alpha**2 + 1./2 * self.sigma_V**2) * np.log( (self.q/self.nu_max**2)**2 * (1 + 2*(self.alpha/self.sigma_V)**2) )
-
-        
-        def selfcon(self):
-            return self.I_nu_squared() - self.I_q_squared() 
+    
+    def selfcon(self):
+        return self.I_nu_squared() - self.I_q_squared() 
 
 
-        class Synapse:
+class Synapse:
 
-            def __init__(self, idx, **kwargs):
-                self.idx = idx
-                self.tau_I = kwargs.get('tau_I', 0.01)
-                self.r_I = kwargs.get('r_I', 1.)
-                self.tau_norm = kwargs.get('tau_norm',1.)
-            
-            def are_values_ok(self):
-                attrs = ['tau_I','r_I','tau_norm']
-                return all(getattr(self, attr) is not None and getattr(self, attr) > 0 for attr in attrs)
+    def __init__(self, population, **kwargs):
 
-            def print_current_state(self):
-                print(f"  Synapse {self.idx}:")
-                print(f"    tau_I: {self.tau_I}")
-                print(f"    r_I: {self.r_I}")
-                print(f"    tau_norm: {self.tau_norm}")
+        self.population = population
+        self.idx = len(population.synapses)
+        self._tau_I = kwargs.get('tau_I', 0.01)
+        self._r_I = kwargs.get('r_I', 1.)
+        self._tau_norm = kwargs.get('tau_norm',1.)
+
+    @property
+    def tau_I(self):
+        return self._tau_I
+    
+    @tau_I.setter
+    def tau_I(self, value):
+        self._tau_I = value
+        self.population.network._recalculate_sigma_V = True
+
+    @property
+    def r_I(self):
+        return self._r_I
+
+    @r_I.setter
+    def r_I(self, value):
+        self._r_I = value
+        self.population.network._recalculate_sigma_V = True
+
+    @property
+    def tau_norm(self):
+        return self._tau_norm
+
+    @tau_norm.setter
+    def tau_norm(self, value):
+        self._tau_norm = value
+        self.population.network._recalculate_sigma_V = True
+
+    def are_values_ok(self):
+        attrs = ['tau_I','r_I','tau_norm']
+        return all(getattr(self, attr) is not None and getattr(self, attr) > 0 for attr in attrs)
+
+    def print_current_state(self):
+        print(f"  Synapse {self.idx}:")
+        print(f"    tau_I: {self.tau_I}")
+        print(f"    r_I: {self.r_I}")
+        print(f"    tau_norm: {self.tau_norm}")